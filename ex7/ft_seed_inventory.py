4473f3f5be057e178722cea3d6df0a54fd1b2c34
Adam Raji <adraji@e1r9p12.1337.ma>
2026-01-03 15:17:21 +0100

    fixe output bag in ex7

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,15 +1,13 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str):
 
-    output_string = seed_type[0].upper() + seed_type[1:].lower()
-
-    elif (unit == "packets" or unit == "grams" or unit == "area"):
+    if (unit == "packets" or unit == "grams" or unit == "area"):
         if (unit == "packets"):
-            last_word = "available"
+            last_word = unit + " available"
         elif (unit == "grams"):
-            last_word = "total"
+            last_word = unit + " total"
         else:
             last_word = "square meters"
-        print(f"{output_string} seeds: {quantity} {unit} {last_word}")
+        print(seed_type.capitalize(), "seeds:", quantity, last_word)
 
     else:
         print("Unknown unit type")
